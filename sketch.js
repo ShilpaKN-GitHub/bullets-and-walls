92dc5d894723ef6302ec5ad2ebf17366003e09c1
ShilpaKN-GitHub <69884599+ShilpaKN-GitHub@users.noreply.github.com>
2020-08-23 23:35:41 +0530

    Add files via upload
    Test the reliability of walls when bullets hit them.

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var wall, thickness;
+var bullet, speed, weight;
+
+function setup()
+{
+	createCanvas(1600, 400);
+
+	thickness = random(22, 83);
+
+	wall = createSprite(1200, 200, thickness, height / 2);  
+	wall.shapeColor = color(80, 80, 80);
+
+	speed = random(223, 321);
+	weight = random(30, 52);
+
+    bullet = createSprite(50, 200, 50, 10);  
+    bullet.velocityX = speed;
+    bullet.shapeColor = "white";
+}
+
+
+function draw()
+{
+	background("black");  
+
+	if(hasCollided(bullet, wall))
+	{
+		bullet.velocityX = 0;
+
+		var damage = (0.5 * weight * speed * speed) / (thickness * thickness * thickness);
+
+		if(damage > 10)
+		{
+			wall.shapeColor = "red";			
+		}
+		else
+		{
+			wall.shapeColor = "green";
+		}
+	}
+
+	drawSprites();
+}
+
+function hasCollided(lbullet, lwall)
+{
+	var bulletRightEdge = lbullet.x + lbullet.width;
+	var wallLeftEdge = lwall.x;
+	
+	if (bulletRightEdge >= wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
